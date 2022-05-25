237266e6c192fade1787f6d750a8ebf7827f885c
galeisan <galeeva.leisan.27.09@yandex.ru>
2022-05-26 00:05:26 +0300

    fixes

--- src/pages/ListsPage/index.tsx
+++ src/pages/ListsPage/index.tsx
@@ -83,10 +83,8 @@ export const ListsPage = observer(() => {
     console.log(currentUser)
 
     return (
-        <ProfileLayout>
-            <div className={styles.content_container}>
-                lists
-            </div>
-        </ProfileLayout>
+        <div className={styles.content_container}>
+            lists
+        </div>
     )
 });
